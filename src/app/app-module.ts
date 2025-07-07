f42458b21475d37b56b2d8060260b10fd8ed1eba
engakhattab <engabdelrahmankhattab@gmail.com>
2025-07-08 01:27:01 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Home
   ],
   imports: [
     BrowserModule,
