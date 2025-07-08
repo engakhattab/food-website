6d72065542b640116a2b5b86f341fb1c33b63012
engakhattab <engabdelrahmankhattab@gmail.com>
2025-07-08 13:15:36 +0300

    Implement header and home components with styling; add FoodService for food data retrieval

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FoodService } from '../../services/food/food-service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+  foods:string[] =[];
+  constructor(private foodService: FoodService) { }
+  ngOnInit() : void {
+    this.foods = this.foodService.getAll();
+  }
 
 }
